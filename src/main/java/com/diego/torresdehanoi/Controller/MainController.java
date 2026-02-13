0c118fd3026027d7c5807f1e879466c167cddd68
Diego0528 <dandrinojimenez@ejemplo.com>
2026-02-13 16:47:13 -0600

    Implementacion de la primera vista y diseño

---
+++ src/main/java/com/diego/torresdehanoi/Controller/MainController.java
@@ -0,0 +1,15 @@
+package com.diego.torresdehanoi.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class MainController {
+
+    @FXML
+    private Button iniciarbtn;
+
+    @FXML
+    private Label lbContador;
+
+}
--- src/main/java/com/diego/torresdehanoi/utils/Paths.java
+++ src/main/java/com/diego/torresdehanoi/utils/Paths.java
@@ -1,5 +1,5 @@
 package com.diego.torresdehanoi.utils;
 
 public class Paths {
-    public static final String MainView = "/com/diego/torresdehanoi/MainView.fxml";
+    public static final String MainView = "/com/diego/torresdehanoi/Views/MainView.fxml";
 }
